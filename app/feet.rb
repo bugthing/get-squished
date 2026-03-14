5803019c850caa6610ae67c564e1efd29aea0921
B Martin <benjamin247365@hotmail.com>
2026-03-14 16:41:56 +0000

    feat: dynamically add feet over time up to maximum of 7

--- app/feet.rb
+++ app/feet.rb
@@ -1,8 +1,10 @@
 class Feet
-  FOOT_COUNT = 3
+  FOOT_COUNT   = 3
+  MAX_FEET     = 7
+  SPAWN_EVERY  = 20  # seconds
 
   def initialize(state)
-    self.state = state
+    self.state       = state
     self.state.feet ||= default_collection
   end
 
@@ -11,6 +13,19 @@ class Feet
   def each_foot
     state.feet.each { |entity| yield(Foot.new(entity)) }
   end
+
+  def update_count(score_seconds)
+    target = [FOOT_COUNT + (score_seconds / SPAWN_EVERY), MAX_FEET].min
+    while state.feet.length < target
+      entity = state.new_entity(:foot)
+      foot   = Foot.new(entity)
+      last   = Foot.new(state.feet.last)
+      foot.defaults!(right_of: last)
+      state.feet << entity
+    end
+  end
+
+  private
 
   def default_collection
     all_new_feet.tap do |collection|
